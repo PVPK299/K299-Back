05d27bbfc805840c3c2f41741baedbfb4f821ffb
Emilis <46862485+Qoldoon@users.noreply.github.com>
2023-03-09 17:15:40 +0200

    Add SolarData object

---
+++ K299-Back/Controllers/SolarDataController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+using System.Text.Json;
+
+namespace K299_Back.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SolarDataController : ControllerBase
+    {
+        private readonly ILogger<SolarDataController> _logger;
+
+        public SolarDataController(ILogger<SolarDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        private void query(string q)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .Build();
+
+            
+            string connectionString = configuration.GetConnectionString("SolarParkConnectionString");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(q, conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            
+                        }
+                    }
+                }
+            }
+        }
+
+        [HttpGet(Name = "GetSolarData")]
+        public IEnumerable<SolarData> Get()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
+
+
---
+++ K299-Back/SolarData.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+namespace K299_Back
+{
+	public class SolarData
+	{
+        public DateTime time { get; set; }
+        public int temperature { get; set; }
+		public float pv1V { get; set; }
+        public float pv2V { get; set; }
+        public float pv1A { get; set; }
+        public float pv2A { get; set; }
+        public float totalEnergy { get; set; }
+        public int totalTime { get; set; }
+        public int totalPower { get; set; }
+        public string? mode { get; set; }
+        public float dailyEnergy { get; set; }
+        //public int pv1Input { get; set; }
+        //public int pv2Input { get; set; }
+        public int heatsinkTemp { get; set; }
+        //public float currentR { get; set; }
+        //public float voltageR { get; set; }
+        //public float frequencyR { get; set; }
+        //public float currentS { get; set; }
+        //public float voltageS { get; set; }
+        //public float frequencyS { get; set; }
+        //public float currentT { get; set; }
+        //public float voltageT { get; set; }
+        //public float frequencyT { get; set; }
+        //public float reducedCO2 { get; set; }
+        //public float reducedSO2 { get; set; }
+        //public float reducedOIL { get; set; }
+        //public float reducedCOAL { get; set; }
+        
+
+    }
+}
+
