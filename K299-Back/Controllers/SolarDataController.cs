29e59dd0271a3d799108e75735a03c04aa7cb9c8
ValPaul <valdas.pauliukevicius2001@gmail.com>
2023-03-12 02:07:23 +0200

    Added GetByIDFromTo API for retrieving solar data from database in a range

--- K299-Back/Controllers/SolarDataController.cs
+++ K299-Back/Controllers/SolarDataController.cs
@@ -23,6 +23,7 @@ namespace K299_Back.Controllers
             _configuration = configuration;
         }
 
+        // GET: api/SolarData/GetSolarData
         [HttpGet("GetSolarData")]
         public JsonResult Get()
         {
@@ -72,6 +73,7 @@ namespace K299_Back.Controllers
             return new JsonResult(Data);
         }
 
+        // GET: api/SolarData/GetSolarDataByID/{ID}
         [HttpGet("GetSolarDataByID/{ID}")]
         public JsonResult GetByID(int ID)
         {
@@ -121,7 +123,57 @@ namespace K299_Back.Controllers
             return new JsonResult(Data);
         }
 
-        // GET: api/SolarData/InsertDataFromFile
+        // GET: api/SolarData/GetByIDFromTo/{IDFrom}/{IDTo}
+        [HttpGet("GetByIDFromTo/{IDFrom}/{IDTo}")]
+        public JsonResult GetByIDFromTo(int IDFrom, int IDTo)
+        {
+            List<SolarData> Data = new List<SolarData>();
+
+            string query = @"SELECT ID, Time, Temperature, PV1_Voltage, PV2_Voltage, PV1_Current,
+                                    PV2_Current, Total_Energy, Total_Operation_Hours, Total_AC_Power,
+                                   Daily_Energy, ControllerName FROM dbo.solar_park WHERE ID >=" + IDFrom + " AND ID <=" + IDTo;
+
+
+            string sqlDataSource = _configuration.GetConnectionString("SolarData");
+
+            SqlDataReader myreader;
+
+
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myreader = myCommand.ExecuteReader();
+                    while (myreader.Read())
+                    {
+                        SolarData solar = new SolarData()
+                        {
+                            ID = (int)myreader.GetInt32(0),
+                            Time = DateTime.Parse(myreader.GetString(1)),
+                            Temperature = (float)myreader.GetDouble(2),
+                            PV1_Voltage = (float)myreader.GetDouble(3),
+                            PV2_Voltage = (float)myreader.GetDouble(4),
+                            PV1_Current = (float)myreader.GetDouble(5),
+                            PV2_Current = (float)myreader.GetDouble(6),
+                            Total_Energy = (float)myreader.GetDouble(7),
+                            Total_Operation_Hours = (float)myreader.GetDouble(8),
+                            Total_AC_Power = (float)myreader.GetDouble(9),
+                            Daily_Energy = (float)myreader.GetDouble(10),
+                            ControllerName = myreader.GetString(11)
+                        };
+                        Data.Add(solar);
+                    }
+                    myreader.Close();
+                }
+
+                myCon.Close();
+            }
+            return new JsonResult(Data);
+        }
+
+        // GET: api/SolarData/InsertDataFromFile/{filename}
         [HttpGet("InsertDataFromFile/{filename}")]
         public JsonResult ReadCsvAndPutData(string filename)
         {
@@ -215,9 +267,6 @@ namespace K299_Back.Controllers
 
             }
         }
-
-        //public void InsertOne(DateTime Time, float Temperature, float PV1_Voltage, float PV2_Voltage, float PV1_Current, float PV2_Current, float Total_Energy,
-        //                        float Total_Operation_Hours, float Total_AC_Power, float Daily_Energy, string ControllerName)
 
         //Put: api/SolarData/IsnertOne
         [HttpPut("InsertOne")]
